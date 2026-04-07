57b0ba4844f1527f38d82ef12454e82d81899dc4
gemaginting <gemaginting1@gmail.com>
2026-04-08 01:21:11 +0700

    update terbaru lagi

--- cinerec/settings.py
+++ cinerec/settings.py
@@ -125,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# cinerec/settings.py
+
+# ... (kode pengaturan lainnya) ...
+
+# Media files (Images, videos, etc.)
--- cinerec/urls.py
+++ cinerec/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings # <-- Tambahkan ini
+from django.conf.urls.static import static # <-- Tambahkan ini
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include ('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include('recomendations.urls')),
 ]
+
+# <-- Tambahkan blok ini di paling bawah
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
